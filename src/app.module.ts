c73558381ba4e8124d30c441d2476561765751ad
Tracey Onim <traceyonim22@gmail.com>
2025-10-20 14:05:00 +0300

    create bill controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BillsController } from './bills/bills.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, BillsController],
   providers: [AppService],
 })
 export class AppModule {}
